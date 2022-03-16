b9226bc4f1e38161641b1f4787684fac6a7402bc
Geuni <dlrmsgnl620@naver.com>
2022-03-16 21:42:56 +0900

    Add: feature/KeunHwee branch 생성 및 파일 업로드

--- src/pages/Keun/Login/Login.js
+++ src/pages/Keun/Login/Login.js
@@ -1,5 +1,30 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.scss';
+
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  navigate('/main');
+  return (
+    <div className="wrap">
+      <div className="logo">westagram</div>
+      <form className="login">
+        <input
+          className="login__email"
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+        />
+        <input className="login__pw" type="password" placeholder="비밀번호" />
+        <button className="login__btn" type="button">
+          로그인
+        </button>
+      </form>
+
+      <footer className="footer">
+        <a>비밀번호를 잊으셨나요?</a>
+      </footer>
+    </div>
+  );
 };
 
 export default Login;
