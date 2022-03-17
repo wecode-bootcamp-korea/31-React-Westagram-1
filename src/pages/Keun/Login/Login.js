782bfbaaebc0b974cf0f7d485390c56a7a4d548e
Geuni <dlrmsgnl620@naver.com>
2022-03-17 13:32:01 +0900

    Modify: gotoMain함수 경로수정

--- src/pages/Keun/Login/Login.js
+++ src/pages/Keun/Login/Login.js
@@ -4,7 +4,10 @@ import './Login.scss';
 
 const Login = () => {
   const navigate = useNavigate();
-  navigate('/main');
+  const gotoMain = () => {
+    navigate('../Keun/Main');
+  };
+
   return (
     <div className="wrap">
       <div className="logo">westagram</div>
@@ -15,7 +18,7 @@ const Login = () => {
           placeholder="전화번호, 사용자 이름 또는 이메일"
         />
         <input className="login__pw" type="password" placeholder="비밀번호" />
-        <button className="login__btn" type="button">
+        <button className="login__btn" type="button" onClick={gotoMain}>
           로그인
         </button>
       </form>
